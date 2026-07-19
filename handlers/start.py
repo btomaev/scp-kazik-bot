df0014bb83d55c668fb5c43f71d80ea3de4ad2bc
btomaev <btomaev34@gmail.com>
2026-07-19 20:34:51 +0300

    Improve /start handler

--- handlers/start.py
+++ handlers/start.py
@@ -9,10 +9,8 @@ async def start(msg: types.Message, user_storage: UserStorage):
 
     if is_first_time:
         start_balance = config.get('start_balance', default=0)
-        await user_storage.set_many({
-            'balance': start_balance,
-            'is_seen': True
-        })
+        await user_storage.increment('balance', start_balance)
+        await user_storage.set('is_seen', True)
         await msg.reply(
             config.get('localization.start.welcome').format(
                 start_balance=start_balance,
